9b004d303ba14520d432bd4f5a25371672ff2926
mustafatekin93 <60111044+mustafatekin93@users.noreply.github.com>
2026-08-15 01:11:02 +0300

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -6,14 +6,14 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class ExamplePlugin: Plugin() {
+class DiziyouPlugin: Plugin() {
     private var activity: AppCompatActivity? = null
 
     override fun load(context: Context) {
         activity = context as? AppCompatActivity
 
         // All providers should be added in this manner
-        registerMainAPI(ExampleProvider())
+        registerMainAPI(DiziyouProvider())
 
         openSettings = {
             val frag = BlankFragment(this)
