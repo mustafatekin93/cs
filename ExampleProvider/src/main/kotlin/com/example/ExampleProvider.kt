f9f229f2626171653b51f9e95f3df516765e2ad6
mustafatekin93 <60111044+mustafatekin93@users.noreply.github.com>
2026-08-15 01:39:44 +0300

    Rename provider from 'Diziyou' to 'Diziyou-Ugumaf'

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.TvType
 
 class DiziyouProvider : MainAPI() { // All providers must be an instance of MainAPI
     override var mainUrl = "https://www.diziyou.one/" 
-    override var name = "Diziyou"
+    override var name = "Diziyou-Ugumaf"
     override val supportedTypes = setOf(TvType.TvSeries)
 
     override var lang = "tr"
