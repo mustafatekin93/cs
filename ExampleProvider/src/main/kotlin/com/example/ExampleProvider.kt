56a16570321e13fb16ab0ba2c190830434599914
mustafatekin93 <60111044+mustafatekin93@users.noreply.github.com>
2026-08-15 00:40:33 +0300

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -5,11 +5,11 @@ import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
 
 class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
+    override var mainUrl = "https://www.diziyou.one/" 
+    override var name = "Diziyou"
     override val supportedTypes = setOf(TvType.Movie)
 
-    override var lang = "en"
+    override var lang = "tr"
 
     // Enable this when your provider has a main page
     override val hasMainPage = true
